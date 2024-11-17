600677526273105efe9f96da8530bfed3c0900fb
Muhammad Rydwan <rydwan.dev@gmail.com>
2024-11-17 13:42:52 +0700

    Initial Commit

---
+++ Hofu/HofuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HofuApp.swift
+//  Hofu
+//
+//  Created by Muhammad Rydwan on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HofuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HofuTests/HofuTests.swift
@@ -0,0 +1,17 @@
+//
+//  HofuTests.swift
+//  HofuTests
+//
+//  Created by Muhammad Rydwan on 17/11/24.
+//
+
+import Testing
+@testable import Hofu
+
+struct HofuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
